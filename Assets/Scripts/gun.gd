cfb0155b7f4fd91d00960af9a0161b9d1dce2800
ID-DOBBY <130461020+ID-DOBBY@users.noreply.github.com>
2024-06-22 01:37:57 +0200

    Added Ammo Count| Added 2nd room| Improved Camera Movement

--- Assets/Scripts/gun.gd
+++ Assets/Scripts/gun.gd
@@ -1,16 +1,24 @@
 extends Node2D
 
+@onready var timer = $Timer
 @onready var crosshair = get_node("/root/Map/crosshair")
 const BULLET = preload("res://Assets/Scenes/bullet.tscn")
 @onready var rotation_point = $Rotation_Point
-
+var ammo = 6
 
 func _process(_delta):
 	look_at(crosshair.global_position)
 	rotation_point.look_at(crosshair.global_position)
 	
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot") and ammo > 0 and timer.is_stopped() == true:
+		ammo -= 1
+		timer.start(0.3)
 		var new_bullet = BULLET.instantiate()
 		new_bullet.global_position = $Bullet_Spawn.global_position
 		new_bullet.global_rotation = $Bullet_Spawn.global_rotation
 		get_parent().get_parent().add_child(new_bullet)
+		
+	if Input.is_action_just_pressed("reload"):
+		await get_tree().create_timer(1.2).timeout
+		ammo = 6
+		
--- Assets/Scripts/map.gd
+++ Assets/Scripts/map.gd
@@ -1,15 +1,56 @@
 extends Node2D
 signal no_enemies
-@onready var enemy = $Enemy
+
+@onready var camera = $TileMap/Camera2D
 @onready var tunnel = $Tunnel
 var i =0
+var current_room = 1
+const ENEMY = preload("res://Assets/Scenes/enemy.tscn")
+@onready var spawn_pos = $Path2D/spawn_pos
+@onready var timer = $Timer
+
+
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	
 
 func _process(delta):
 	
-	if find_child("Enemy") == null and i == 0:
-		tunnel.queue_free()
-		i+=1
+	var ammo_label = camera.get_child(0).get_child(0)
+	var text = "AMMO: " + str(%Player/Gun.ammo)
+	ammo_label.text = (text)
+	match current_room:
+		1:
+			camera.global_position = global_position
+			if $Enemies.get_child_count() == 0 and find_child("Tunnel"):
+				tunnel.queue_free()
+				
+		null:
+			camera.global_position = %Player.global_position
+			camera.position_smoothing_enabled = true
+		2:
+			if timer.is_stopped() == true and i <5:
+				timer.start(0.2)
+				spawn_pos.progress_ratio = randf()
+				var new_enemy = ENEMY.instantiate()
+				new_enemy.global_position = spawn_pos.global_position
+				find_child("Enemies").add_child(new_enemy)
+				i+=1
+			camera.global_position = %Player.global_position
+			camera.position_smoothing_enabled = true
+	
+	
+	
 		
+		
+
+
+func _on_room_2_area_entered(area):
+	current_room = 2
+	
+	
+
+
+func _on_room_1_area_entered(area):
+	current_room = 1
